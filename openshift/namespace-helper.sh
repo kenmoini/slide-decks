b8f46a65a9ac2cad7f8d8af5a7d759764869e5c0
Ken Moini <ken@kenmoini.com>
2019-06-08 03:11:14 -0500

    Update namespace-helper.sh

--- openshift/namespace-helper.sh
+++ openshift/namespace-helper.sh
@@ -4,8 +4,8 @@ oc new-project test-sites-slides-fiercesw-network --display-name="[TEST][Site] F
 oc new-project stage-sites-slides-fiercesw-network --display-name="[STAGE][Site] Fierce Network Slides"
 oc new-project sites-slides-fiercesw-network --display-name="[PROD][Site] Fierce Network Slides"
 
-oc policy add-role-to-user edit system:serviceaccount:integrated-cicd:jenkins -n test-sites-slides-fiercesw-network
-oc policy add-role-to-user edit system:serviceaccount:integrated-cicd:jenkins -n stage-sites-slides-fiercesw-network
-oc policy add-role-to-user edit system:serviceaccount:integrated-cicd:jenkins -n sites-slides-fiercesw-network
+oc policy add-role-to-user edit system:serviceaccount:integrated-cicd:jenkins -n test-site-slides-fiercesw-network
+oc policy add-role-to-user edit system:serviceaccount:integrated-cicd:jenkins -n stage-site-slides-fiercesw-network
+oc policy add-role-to-user edit system:serviceaccount:integrated-cicd:jenkins -n site-slides-fiercesw-network
 
 oc adm pod-network join-projects --to=integrated-cicd test-site-slides-fiercesw-network stage-site-slides-fiercesw-network site-slides-fiercesw-network >/dev/null 2>&1
